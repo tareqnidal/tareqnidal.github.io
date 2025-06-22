eea620d5e8db1e5eca36d830c5b8e62c9e92d80e
Tareq Nassrallah <tnn992011@hotmail.com>
2025-06-22 23:47:16 +0200

    vite retry

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', 
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/', // your repo is tareqnidal.github.io
+  build: {
+    outDir: 'docs', // 👈 this is key
   },
+  plugins: [react()],
 });
+
 
