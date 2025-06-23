cccda8dd8969ffca26fedbcb4af14e1ada6fa2f8
Tareq Nassrallah <tnn992011@hotmail.com>
2025-06-23 21:19:01 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repoName = process.env.VITE_REPO_NAME || '';
+
 export default defineConfig({
-  base: '/', 
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
 });
-
-
